1a78bd569f22b5725a9a6b9427bb69d032946a57
longxing <chenfeilong93@163.com>
2014-07-06 18:18:01 +0800

    tij
    Signed-off-by: longxing <chenfeilong93@163.com>

---
+++ ServiceManagement/files/短信平台/data.js
@@ -0,0 +1,36 @@
+﻿$axure.loadCurrentPage({
+  "url":"短信平台.html",
+  "generationDate":new Date(1401345416710.71),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable"],
+  "page":{
+    "packageId":"bf82438a75c845718bf14f67537fbd97",
+    "type":"Axure:Page",
+    "name":"短信平台",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"应用字体",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+},
+    "diagram":{
+      "objects":[]}},
+  "masters":{
+},
+  "objectPaths":{
+}});
